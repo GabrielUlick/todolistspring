3e80bc97a0226dad4cdc9da0d4e9991fbf1e9b61
Gabriel Ulick <gabrieludb@hotmail.com>
2023-10-13 22:52:11 -0300

    first commit

---
+++ src/main/java/br/com/gabrielulick/todolist/task/ITaskRepository.java
@@ -0,0 +1,10 @@
+package br.com.gabrielulick.todolist.task;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {
+    List<TaskModel> findByIdUser(UUID idUser);
+}
---
+++ src/main/java/br/com/gabrielulick/todolist/user/UserModel.java
@@ -0,0 +1,37 @@
+package br.com.gabrielulick.todolist.user;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity(name = "tb_users")
+public class UserModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
+
+    @Column(name = "usuario", unique = true)
+    private String username;
+
+    @Column(name = "nome_usuario")
+    private String name;
+
+    @Column(name = "senha")
+    private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+}
+
+
